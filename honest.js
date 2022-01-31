62034c9e9702b0fc5b6cb810452823068925c1a2
bythebootstrap <48031236+bythebootstrap@users.noreply.github.com>
2022-01-31 09:25:29 -0800

    Update honest.js
    removed console log for popup param

--- honest.js
+++ honest.js
@@ -5,7 +5,6 @@
 //Create objects from query parameters
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-console.log(params.popup)
 
 
 
